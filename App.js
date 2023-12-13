f2ac8d2244eba6e169026b9b6117435e230f033f
thisissabhariesh <117589228+thisissabhariesh@users.noreply.github.com>
2023-12-13 14:04:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div>
+      {/* <Login /> */}
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
